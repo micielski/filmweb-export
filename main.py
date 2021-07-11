97cff45d7529db5743b467d490861627397b395d
xrew <r.micielski@hotmail.com>
2021-07-11 19:19:42 +0200

    finished logging in

--- main.py
+++ main.py
@@ -27,17 +27,19 @@ browser.find_element_by_xpath("//*[@id=\"main-header_login-link\"]").click()
 browser.find_element_by_xpath("//*[@id=\"site\"]/div[2]/div/div/div[1]/div/div/ul/li[3]/div").click()
 browser.find_element_by_name("j_username").send_keys(username)
 browser.find_element_by_name("j_password").send_keys(password)
+browser.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/form/div[2]/ul/li[1]/button").click()
+time.sleep(3)
 
-
-'''
 for x in f:
-    ocena = "".join(re.findall(r"(\d+)/", x))
+    rating = "".join(re.findall(r"(\d+)/", x))
     x = re.sub("[0-9]+/\d\d$", "", x)
     x = re.sub("\s+$", "", x)
     x = x.replace(" ", "+")
     browser.get("https://www.filmweb.pl/search?q=" + x)
     browser.find_element_by_class_name("filmPreview__link").click()
     time.sleep(5)
-    browser.find_element_by_xpath("/html/body/div[4]/div[3]/div[2]/div/div[3]/section/div/div/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/a[" + ocena + "]").click()
+    browser.find_element_by_xpath("/html/body/div[4]/div[3]/div[2]/div/div[3]/section/div/div/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/a[" + rating + "]").click()
     print("Done executing")
-    time.sleep(1)'''
+    time.sleep(1)
+
+print("done baby :) :3")
