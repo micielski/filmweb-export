9e40f573fb4d65ccfb92a737e911819e7384e435
xrew11 <r.micielski@hotmail.com>
2021-07-24 13:21:38 +0200

    initial ratings parser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import configparser
 import re
 import time
@@ -6,29 +7,28 @@ import time
 settings = configparser.ConfigParser()
 settings.read("settings.cfg")
 
-chromedriver = settings.get("settings", "chromedriver")
-movies = settings.get("settings", "list")
-username = settings.get("settings", "username")
-password = settings.get("settings", "password")
-timeout = settings.get("settings", "timeout")
-loginTimeout = int(settings.get("settings", "login_timeout"))
+userVariables = {}
+for i in ["chromedriver", "movies", "username", "password", "timeout"]:
+    userVariables[i] = settings.get("settings", i)
 
-driver = webdriver.Chrome(chromedriver)
-driver.implicitly_wait(timeout)
+driver = webdriver.Chrome(userVariables["chromedriver"])
+driver.implicitly_wait(userVariables["timeout"])
 
-movies = open(movies, "r")
+movies = open(userVariables["movies"], "r")
 
 driver.get("https://www.filmweb.pl/")
 driver.find_element_by_id("didomi-notice-agree-button").click()
 
+
 def filmweb_login():
     driver.get("https://www.filmweb.pl/login")
     driver.find_element_by_xpath("//*[@id=\"site\"]/div[2]/div/div/div[1]/div/div/ul/li[3]/div").click()
-    driver.find_element_by_name("j_username").send_keys(username)
-    driver.find_element_by_name("j_password").send_keys(password)
+    driver.find_element_by_name("j_username").send_keys(userVariables["username"])
+    driver.find_element_by_name("j_password").send_keys(userVariables["password"])
     time.sleep(loginTimeout/2)
     driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/form/div[2]/ul/li[1]/button").click()
     time.sleep(loginTimeout/2)
+
 
 def txt_to_filmweb():
     for title in movies:
@@ -41,7 +41,18 @@ def txt_to_filmweb():
         driver.find_element_by_xpath("//*[@id=\"site\"]/div[3]/div[3]/div/div[1]").click()
         driver.find_element_by_xpath("//*[@id=\"site\"]/div[3]/div[3]/div/div[2]/div/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/div/a[" + rating + "]").click()
 
-filmweb_login()
-txt_to_filmweb()
 
-  
+def filmweb_Import():
+    driver.get("https://www.filmweb.pl/user/" + userVariables["username"] + "/films")
+    html = driver.page_source
+    soup = BeautifulSoup(html, "lxml")
+    
+    ratings = soup.find_all(class_="userRate__rate")
+
+    for rating in ratings:
+        print(rating.text)
+
+# filmweb_login()
+# txt_to_filmweb()
+
+filmweb_Import()
